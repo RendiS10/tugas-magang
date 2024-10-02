956c794102073c9a0e51ef9ac5af0313678baa55
RendiS10 <rendisutendi10@gmail.com>
2024-10-02 13:59:30 +0700

    menambahkan fitur login

---
+++ controller/login-validation.js
@@ -0,0 +1,28 @@
+function validateForm() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  let errorMessage = '';
+
+  // Validasi username/email
+  if (!username) {
+    errorMessage += 'Username/Email harus diisi.\n';
+  }
+
+  // Validasi password
+  if (!password) {
+    errorMessage += 'Password harus diisi.\n';
+  }
+  if (errorMessage) {
+    alert(errorMessage);
+    return false;
+  }
+
+  // Cek Username dan Password
+  if (username === 'admin@gmail.com' && password === 'admin123') {
+    window.location.href = 'admin.html';
+    return false; // Mencegah form dari submit
+  } else {
+    alert('Username atau password salah.');
+    return false;
+  }
+}
---
+++ data/data.js
@@ -0,0 +1,5 @@
+const data = [
+  "sumber" = "https://uinjkt.ac.id/index.php/id/dr-muhbib-abdul-wahhab-bahasa-arab-amiyah-perlu-diajarkan",
+  "img" = "",
+  "tittle" = "Pentingnya Belajar Bahasa Arab"
+]
