d4fe569ba42012c76c9629a4145eb6db3b19deab
Rendi Sutendi <130779841+RendiS10@users.noreply.github.com>
2024-10-05 14:01:59 +0700

    Update regist.js

--- controller/regist.js
+++ controller/regist.js
@@ -1,38 +1,59 @@
 function validateRegist() {
-  const nama = document.getElementById('nama').value;
-  const alamat = document.getElementById('alamat').value;
-  const gender = document.getElementById('gender').value;
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-  let errorMessage = '';
 
-  if (!nama || !alamat || !gender || !username || !password) {
-    // Validasi nama
-    if (!nama) {
-      errorMessage += 'Nama harus diisi.\n';
-    }
-    // Validasi alamat
-    if (!alamat) {
-      errorMessage += 'Alamat harus diisi.\n';
-    }
-    // Validasi gender
-    if (!gender) {
-      errorMessage += 'Gender harus diisi.\n';
-    }
-    // Validasi username/email
-    if (!username) {
-      errorMessage += 'Username/Email harus diisi.\n';
-    }
-    // Validasi password
-    if (!password) {
-      errorMessage += 'Password harus diisi.\n';
-    }
-    
-    if (errorMessage) {
-      alert(errorMessage);
-      return false;
-    }
-  } else {
-    window.location.href = 'login.html';
-  }
-}
+     const nama = document.getElementById('nama').value;
+
+     const alamat = document.getElementById('alamat').value;
+
+     const gender = document.getElementById('gender').value;
+
+     const username = document.getElementById('username').value;
+
+     const password = document.getElementById('password').value;
+
+     let errorMessage = '';
+
+     if (!nama) {
+
+       errorMessage += 'Nama harus diisi.\n';
+
+     }
+
+     if (!alamat) {
+
+       errorMessage += 'Alamat harus diisi.\n';
+
+     }
+
+     if (!gender) {
+
+       errorMessage += 'Gender harus diisi.\n';
+
+     }
+
+     if (!username) {
+
+       errorMessage += 'Username/Email harus diisi.\n';
+
+     }
+
+     if (!password) {
+
+       errorMessage += 'Password harus diisi.\n';
+
+     }
+
+     if (errorMessage) {
+
+       alert(errorMessage);
+
+       return false;
+
+     } else {
+
+       event.preventDefault();
+
+       window.location.href = 'login.html';
+
+     }
+
+   }
